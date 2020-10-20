be06e62961b519ea0042749ae914134400a8d02d
Petr Faltus <petrfaltus@petrfaltus.net>
2020-10-20 09:59:57 +0200

    Place the application to the real center of the screen

--- src/main/java/cz/petrfaltus/namedayrestclient/NameDayRestClient.java
+++ src/main/java/cz/petrfaltus/namedayrestclient/NameDayRestClient.java
@@ -1,13 +1,28 @@
 package cz.petrfaltus.namedayrestclient;
 
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
 
 public class NameDayRestClient {
+    private static Point getLeftUpperCorner() {
+        Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = defaultToolkit.getScreenSize();
+
+        int leftUpperCornerX = (int)((screenSize.getWidth() - Const.APP_WINDOW_WIDTH) / 2);
+        int leftUpperCornerY = (int)((screenSize.getHeight() - Const.APP_WINDOW_HEIGHT) / 2);
+
+        Point leftUpperCorner = new Point(leftUpperCornerX, leftUpperCornerY);
+
+        return leftUpperCorner;
+    }
+
     public static void main(String[] args) {
+        Point leftUpperCorner = getLeftUpperCorner();
         Dimension preferredSize = new Dimension(Const.APP_WINDOW_WIDTH, Const.APP_WINDOW_HEIGHT);
 
         Gui window = new Gui("Name day REST client");
-        window.setLocationRelativeTo(null); // place to the middle of the screen
+        window.setLocation(leftUpperCorner); // place to the middle of the screen
         window.setPreferredSize(preferredSize);
         window.pack();
         window.setDefaultCloseOperation(Gui.EXIT_ON_CLOSE);
